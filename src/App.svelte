ee9d6885d97e445d8a56eea7e6c7bbe2267d4ac9
gloer <gloer@langslet.com>
2020-03-17 19:49:44 +0100

    Added message

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let name;
+	let message = "This is SVELTE!!!"
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2>{message}</h2>
 </main>
 
 <style>
